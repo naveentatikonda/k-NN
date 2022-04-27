a2478c9654bdc5091281c72cc5be74570e619d9f
Naveen Tatikonda <navtat@amazon.com>
2022-04-27 13:24:02 -0500

    Address review comments
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- qa/restart-upgrade/src/test/java/org/opensearch/knn/bwc/PainlessScriptScoringIT.java
+++ qa/restart-upgrade/src/test/java/org/opensearch/knn/bwc/PainlessScriptScoringIT.java
@@ -1,12 +1,6 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- *
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
+ *  Copyright OpenSearch Contributors
+ *  SPDX-License-Identifier: Apache-2.0
  */
 
 package org.opensearch.knn.bwc;
@@ -15,21 +9,21 @@ public class PainlessScriptScoringIT extends AbstractRestartUpgradeTestCase {
     private static final String TEST_FIELD = "test-field";
     private static final int DIMENSIONS = 5;
     private static final int K = 5;
-    private static final int ADD_DOCS_CNT = 10;
+    private static final int ADD_DOCS_COUNT = 10;
 
     // KNN painless script scoring for space_type "l2"
     public void testKNNL2PainlessScriptScore() throws Exception {
         if (isRunningAgainstOldCluster()) {
             createKnnIndex(testIndex, getKNNScriptScoreSettings(), createKnnIndexMapping(TEST_FIELD, DIMENSIONS));
-            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, ADD_DOCS_CNT);
+            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, ADD_DOCS_COUNT);
         } else {
-            String source = generateL2PainlessScriptSource(TEST_FIELD, DIMENSIONS, ADD_DOCS_CNT);
-            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, ADD_DOCS_CNT, K);
+            String source = generateL2PainlessScriptSource(TEST_FIELD, DIMENSIONS, ADD_DOCS_COUNT);
+            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, ADD_DOCS_COUNT, K);
 
-            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 10, ADD_DOCS_CNT);
+            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 10, ADD_DOCS_COUNT);
 
-            source = generateL2PainlessScriptSource(TEST_FIELD, DIMENSIONS, 2 * ADD_DOCS_CNT);
-            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, 2 * ADD_DOCS_CNT, K);
+            source = generateL2PainlessScriptSource(TEST_FIELD, DIMENSIONS, 2 * ADD_DOCS_COUNT);
+            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, 2 * ADD_DOCS_COUNT, K);
             deleteKNNIndex(testIndex);
         }
     }
@@ -38,15 +32,15 @@ public class PainlessScriptScoringIT extends AbstractRestartUpgradeTestCase {
     public void testKNNL1PainlessScriptScore() throws Exception {
         if (isRunningAgainstOldCluster()) {
             createKnnIndex(testIndex, getKNNScriptScoreSettings(), createKnnIndexMapping(TEST_FIELD, DIMENSIONS));
-            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, ADD_DOCS_CNT);
+            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 0, ADD_DOCS_COUNT);
         } else {
-            String source = generateL1PainlessScriptSource(TEST_FIELD, DIMENSIONS, ADD_DOCS_CNT);
-            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, ADD_DOCS_CNT, K);
+            String source = generateL1PainlessScriptSource(TEST_FIELD, DIMENSIONS, ADD_DOCS_COUNT);
+            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, ADD_DOCS_COUNT, K);
 
-            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 10, ADD_DOCS_CNT);
+            addKNNDocs(testIndex, TEST_FIELD, DIMENSIONS, 10, ADD_DOCS_COUNT);
 
-            source = generateL1PainlessScriptSource(TEST_FIELD, DIMENSIONS, 2 * ADD_DOCS_CNT);
-            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, 2 * ADD_DOCS_CNT, K);
+            source = generateL1PainlessScriptSource(TEST_FIELD, DIMENSIONS, 2 * ADD_DOCS_COUNT);
+            validateKNNPainlessScriptScoreSearch(testIndex, TEST_FIELD, source, 2 * ADD_DOCS_COUNT, K);
             deleteKNNIndex(testIndex);
         }
     }
