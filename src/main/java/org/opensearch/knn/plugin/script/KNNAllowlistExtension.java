9e070647ddb616fadaa7ff9d9c4c74320e065593
Naveen Tatikonda <navtat@amazon.com>
2022-07-11 15:54:02 -0500

    Nomenclature change from Whitelist to Allowlist
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- src/main/java/org/opensearch/knn/plugin/script/KNNAllowlistExtension.java
+++ src/main/java/org/opensearch/knn/plugin/script/KNNAllowlistExtension.java
@@ -7,8 +7,8 @@ package org.opensearch.knn.plugin.script;
 
 import com.google.common.collect.ImmutableMap;
 import org.opensearch.painless.spi.PainlessExtension;
-import org.opensearch.painless.spi.Whitelist;
-import org.opensearch.painless.spi.WhitelistLoader;
+import org.opensearch.painless.spi.Allowlist;
+import org.opensearch.painless.spi.AllowlistLoader;
 import org.opensearch.script.ScoreScript;
 import org.opensearch.script.ScriptContext;
 import org.opensearch.script.ScriptedMetricAggContexts;
@@ -16,13 +16,13 @@ import org.opensearch.script.ScriptedMetricAggContexts;
 import java.util.List;
 import java.util.Map;
 
-public class KNNWhitelistExtension implements PainlessExtension {
+public class KNNAllowlistExtension implements PainlessExtension {
 
-    private static final Whitelist ALLOW_LIST = WhitelistLoader.loadFromResourceFiles(KNNWhitelistExtension.class, "knn_whitelist.txt");
+    private static final Allowlist ALLOW_LIST = AllowlistLoader.loadFromResourceFiles(KNNAllowlistExtension.class, "knn_allowlist.txt");
 
     @Override
-    public Map<ScriptContext<?>, List<Whitelist>> getContextWhitelists() {
-        final List<Whitelist> allowLists = List.of(ALLOW_LIST);
+    public Map<ScriptContext<?>, List<Allowlist>> getContextAllowlists() {
+        final List<Allowlist> allowLists = List.of(ALLOW_LIST);
         return ImmutableMap.of(
             ScoreScript.CONTEXT,
             allowLists,
