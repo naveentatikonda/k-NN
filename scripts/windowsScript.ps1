b18d7561339f08d4a0e629cd4994868b3c5ce60d
Naveen <navtat@amazon.com>
2022-09-21 21:46:55 +0000

    Add Git Patches and Script for Windows Support
    Signed-off-by: Naveen <navtat@amazon.com>

---
+++ scripts/windowsScript.ps1
@@ -0,0 +1,11 @@
+git submodule update --init -- jni/external/nmslib
+git submodule update --init -- jni/external/faiss
+
+git config --global user.email navtat@amazon.com
+git config --global user.name Naveen Tatikonda
+cat patches/CMakeLists.patch | git am
+
+(Get-Content jni/external/faiss/faiss/impl/index_read.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_read.cpp
+(Get-Content jni/external/faiss/faiss/impl/index_write.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_write.cpp
+
+(Get-Content jni/external/faiss/faiss/OnDiskInvertedLists.cpp).replace('#include <sys/mman.h>', "#ifndef __MINGW32__`n#include <sys/mman.h>`n#endif") | Set-Content jni/external/faiss/faiss/OnDiskInvertedLists.cpp
