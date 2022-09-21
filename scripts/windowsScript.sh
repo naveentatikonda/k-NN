9ac9222d615d8544f1f7c920ffd4dbd583f77347
Naveen <navtat@amazon.com>
2022-09-21 21:46:55 +0000

    Add Windows Support
    Signed-off-by: Naveen <navtat@amazon.com>

---
+++ scripts/windowsScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -ex
+
+# Pull library submodule explicitly. While "cmake ." actually pulls the submodule if its not there, we
+# need to pull it before calling cmake. Also, we need to call it from the root git directory.
+# Otherwise, the submodule update call may fail on earlier versions of git.
+git submodule update --init -- jni/external/nmslib
+git submodule update --init -- jni/external/faiss
+
+git apply patches/CMakeLists.patch
+sed -i 's/ _MSC_VER/ __MINGW32__/g' jni/external/faiss/faiss/impl/index_read.cpp
+sed -i 's/ _MSC_VER/ __MINGW32__/g' jni/external/faiss/faiss/impl/index_write.cpp
+sed -i -e 's/#include <sys\/mman.h>/#ifndef __MINGW32__\n#include <sys\/mman.h>\n#endif/' jni/external/faiss/faiss/OnDiskInvertedLists.cpp
