a7b2f5771182d62d1c375b050ac5adc701bac9a0
Naveen <navtat@amazon.com>
2022-09-21 21:46:55 +0000

    Add Git Patches and Script for Windows Support
    Signed-off-by: Naveen <navtat@amazon.com>

---
+++ scripts/wrapper.bat
@@ -0,0 +1,4 @@
+SETLOCAL
+set PATH=%PATH%;C:\Users\Administrator\Downloads\KNN-script-test\test2\k-NN\dependencies
+::./gradlew build
+::./gradlew ':integTest' --tests "org.opensearch.knn.recall.RecallTestsIT"
