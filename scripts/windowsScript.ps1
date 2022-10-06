53fc9764a75b1d70868a33eaadd37ddd39bac6bf
Peter Zhu <zhujiaxi@amazon.com>
2022-10-05 20:04:30 -0400

    Test Windows
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- scripts/windowsScript.ps1
+++ scripts/windowsScript.ps1
@@ -3,7 +3,9 @@ git submodule update --init -- jni/external/faiss
 
 git config --global user.email navtat@amazon.com
 git config --global user.name Naveen Tatikonda
-cat patches/CMakeLists.patch | git am
+#cat patches/CMakeLists.patch | git am
+
+git apply patches/CMakeLists.patch --verbose
 
 (Get-Content jni/external/faiss/faiss/impl/index_read.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_read.cpp
 (Get-Content jni/external/faiss/faiss/impl/index_write.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_write.cpp
