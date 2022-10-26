64e038b1f71e62681457856ceb80f093d952799e
Naveen <navtat@amazon.com>
2022-10-26 04:26:11 +0000

    Remove Git Patch and add branching logic for CMakeLists.txt
    Signed-off-by: Naveen <navtat@amazon.com>

--- scripts/windowsScript.ps1
+++ scripts/windowsScript.ps1
@@ -6,13 +6,8 @@
 git submodule update --init -- jni/external/nmslib
 git submodule update --init -- jni/external/faiss
 
-git apply patches/windows/CMakeLists.patch --verbose
-
-# Validating if the CMakeLists patch is applied
-echo "Validating if the CMakeLists patch is applied"
-type jni/CMakeLists.txt | Select-String "Windows"
-
-# Replace '_MSC_VER' with '__MINGW32__'
+# _MSC_VER is a predefined macro which defines the version of Visual Studio Compiler
+# As we are using x86_64-w64-mingw32-gcc compiler we need to replace this macro with __MINGW32__
 (Get-Content jni/external/faiss/faiss/impl/index_read.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_read.cpp
 (Get-Content jni/external/faiss/faiss/impl/index_write.cpp).replace('_MSC_VER', '__MINGW32__') | Set-Content jni/external/faiss/faiss/impl/index_write.cpp
 
