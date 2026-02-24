ef79793f8e0b57cbce6823557ec5dce04d018bdf
Naveen Tatikonda <navtat@amazon.com>
2026-02-23 23:32:40 -0800

    Address review comments
    Signed-off-by: Naveen Tatikonda <navtat@amazon.com>

--- src/test/java/org/opensearch/knn/integ/NestedSearchIT.java
+++ src/test/java/org/opensearch/knn/integ/NestedSearchIT.java
@@ -148,6 +148,31 @@ public class NestedSearchIT extends KNNRestTestCase {
         assertEquals("13", parseIds(entity).get(1));
     }
 
+    @SneakyThrows
+    public void testNestedSearchWithLuceneOnDisk_whenExpandNestedAndRescoreEnabled_thenSucceed() {
+        createKnnIndex(2, KNNEngine.LUCENE.getName(), Mode.ON_DISK);
+
+        int totalDocCount = 15;
+        for (int i = 0; i < totalDocCount; i++) {
+            String doc = NestedKnnDocBuilder.create(FIELD_NAME_NESTED)
+                .addVectors(FIELD_NAME_VECTOR, new Float[] { (float) i, (float) i }, new Float[] { (float) i, (float) i })
+                .build();
+            addKnnDoc(INDEX_NAME, String.valueOf(i), doc);
+        }
+
+        refreshIndex(INDEX_NAME);
+        forceMergeKnnIndex(INDEX_NAME);
+
+        Float[] queryVector = { 14f, 14f };
+        Response response = queryNestedField(INDEX_NAME, 2, queryVector, null, null, null, 2.0f);
+
+        String entity = EntityUtils.toString(response.getEntity());
+        assertEquals(2, parseHits(entity));
+        assertEquals(2, parseTotalSearchHits(entity));
+        assertEquals("14", parseIds(entity).get(0));
+        assertEquals("13", parseIds(entity).get(1));
+    }
+
     /**
      * {
      * 	"query": {
